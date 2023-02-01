87f343d06d9f5551af4a963c0538142207cd4c79
mariontan <marionltan@gmail.com>
2023-02-01 12:18:33 +0800

    use correct library

--- handler.py
+++ handler.py
@@ -1,9 +1,9 @@
 
-import sys
+# import sys
 # sys.path.insert(0, 'vendor')
-import json
+# import json
 from fastapi import FastAPI
-from magnum import Magnum
+from mangum import Mangum
 
 app = FastAPI()
 
@@ -13,26 +13,26 @@ async def testRoute():
     return {"message": "Hello world"}
 
 
-handler = Magnum(app)
+handler = Mangum(app)
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+# def hello(event, context):
+#     body = {
+#         "message": "Go Serverless v1.0! Your function executed successfully!",
+#         "input": event
+#     }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+#     response = {
+#         "statusCode": 200,
+#         "body": json.dumps(body)
+#     }
 
-    return response
+#     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    # """
+#     # Use this code if you don't use the http event with the LAMBDA-PROXY
+#     # integration
+#     """
+#     return {
+#         "message": "Go Serverless v1.0! Your function executed successfully!",
+#         "event": event
+#     }
+#     # """
